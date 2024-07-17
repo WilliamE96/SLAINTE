2f1bd5eaa539ff8d6bc052ece9e169ffe8b1a72b
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-17 18:22:34 +0100

    set up relationships between models, could need some editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+  has_many :matches
+  has_many :received_matches, class_name: 'Match', foreign_key: 'matched_user_id'
+
+  #sent by user
+  has_many :sent_requests, class_name: "Request", foreign_key: 'sender_id'
+  #sent from user
+  has_many :received_requests, class_name: "Request", foreign_key: 'receiver_id'
 end
