511b820611b1f79c3ca739c963e98b94fc0a0f9b
Sabatino Catucci <sabatinocatucci@hotmail.com>
2024-07-19 10:13:39 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # User routes
   resources :users, only: [:new, :create, :show, :edit, :update]
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+  # Post routes
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+  end
+
+  # Likes routes
+  resources :likes, only: [:index, :show]
+
+  # Match routes
+  resources :matches, only: [:index, :create, :update, :destroy]
+
+  # Request routes
+  resources :requests, only: [:index, :create, :update, :destroy]
 end
