3cc5fdafc1c58f6c81c3272d6e14f91c0ce5f39d
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-20 12:28:34 +0100

    dropped the likes table from the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_200327) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_20_112553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
 
   create_table "matches", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -56,8 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_18_200327) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "matches", "users"
   add_foreign_key "matches", "users", column: "matched_user_id"
   add_foreign_key "posts", "users"
