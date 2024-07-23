f2f3215efe4c768060bdb8996c9a22a730190e64
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-23 16:10:57 +0100

    changed some items in the navbar to re-include the logout funtionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Match', foreign_key: 'matched_user_id'
   has_many :matches, class_name: 'Match', foreign_key: 'matched_user_id'
   has_many :received_matches, class_name: 'Match', foreign_key: 'user_id'
+  has_one_attached :profile_picture
   # hasy_many :asked_matches, class_name: 'Match', foreign_key: 'user_id'
   # #sent by user
   # has_many :sent_requests, class_name: "Request", foreign_key: 'sender_id'
