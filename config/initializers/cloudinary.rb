cfb571a40e78a11c32ce576c470776f928c8ae1a
Sabatino Catucci <sabatinocatucci@hotmail.com>
2024-07-25 19:49:31 +0100

    cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,24 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV['dah2rdyj2']
-  config.api_key = ENV['966843749976385']
-  config.api_secret = ENV['C6vSDhEPBW1GrRyijFJcY8WA1UM']
-  config.secure = true
-end
+# Cloudinary.config do |config|
+#   config.cloud_name = ENV['dah2rdyj2']
+#   config.api_key = ENV['966843749976385']
+#   config.api_secret = ENV['C6vSDhEPBW1GrRyijFJcY8WA1UM']
+#   config.secure = true
+# end
+
+# require 'dotenv'
+# Dotenv.load
+
+# puts "CLOUDINARY_CLOUD_NAME: #{ENV['CLOUDINARY_CLOUD_NAME']}"
+# puts "CLOUDINARY_API_KEY: #{ENV['CLOUDINARY_API_KEY']}"
+# puts "CLOUDINARY_API_SECRET: #{ENV['CLOUDINARY_API_SECRET']}"
+
+# Cloudinary.config do |config|
+#   config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+#   config.api_key = ENV['CLOUDINARY_API_KEY']
+#   config.api_secret = ENV['CLOUDINARY_API_SECRET']
+#   config.secure = true
+# end
+
+# puts "Cloud Name: #{Cloudinary.config.cloud_name}"
+# puts "API Key: #{Cloudinary.config.api_key}"
+# puts "API Secret: #{Cloudinary.config.api_secret}"
