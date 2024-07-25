6ea494bba6523d07c096466c463b121b690a4f10
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-25 20:18:48 +0100

    profile picture working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email, :password_confirmation, :profile_picture)
+    params.require(:user).permit(:username, :password, :email, :password_confirmation, :photo)
   end
 end
