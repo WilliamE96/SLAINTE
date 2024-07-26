adf21a8597645266d56cb7246316b640dd416c2c
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-26 16:27:52 +0100

    I have added a temporary image seed that uses local images from the assets pipeline

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 
+Post.destroy_all
 User.destroy_all
 
 users = [
@@ -21,28 +22,53 @@ user4 = User.find_by(email: 'user4@example.com')
 user5 = User.find_by(email: 'user5@example.com')
 user6 = User.find_by(email: 'user6@example.com')
 
-photo_ids = [
-  'pexels-cottonbro-4781422_xrfwon',
-  'pexels-shkrabaanthony-4612246_ytbxrc',
-  'pexels-ron-lach-8922193_tnvov0',
-  'pexels-cottonbro-6530950_udruta',
-  'pexels-cottonbro-6530941_njnqhc',
-  'pexels-cottonbro-6530928_dxgs9h',
-  'pexels-cottonbro-5530167_nv3rg6',
-  'pexels-cottonbro-5530172_tzvinm',
-  'pexels-cottonbro-5097359_rlll7t',
-  'pexels-cottonbro-4781456_gdvxrc'
-]
-
-def attach_photo(post, photo_id)
-  photo_url = "https://www.pexels.com/photo/#{photo_id}"
-  photo_file = URI.open(photo_url)
-  post.photo.attach(io: photo_file, filename: "#{photo_id}.jpg")
+def attach_photo(post, filename)
+  photo_path = Rails.root.join('app/assets/images/night', filename)
+  post.photo.attach(io: File.open(photo_path), filename: filename)
 end
 
-user1.posts.create!(content: 'First post!', photo: )
-user2.posts.create!(content: 'First post!', photo: )
-user3.posts.create!(content: 'First post!', photo: )
-user4.posts.create!(content: 'First post!', photo: )
-user5.posts.create!(content: 'First post!', photo: )
-user6.posts.create!(content: 'First post!', photo: )
+photo_filenames = [
+  'night1.jpg',
+  'night2.jpg',
+  'night3.jpg',
+  'night4.jpg',
+  'night5.jpg',
+  'night6.jpg'
+]
+
+user1.posts.create!(content: 'First post!').tap { |post| attach_photo(post, photo_filenames[0]) }
+user2.posts.create!(content: 'First post!').tap { |post| attach_photo(post, photo_filenames[1]) }
+user3.posts.create!(content: 'First post!').tap { |post| attach_photo(post, photo_filenames[2]) }
+user4.posts.create!(content: 'First post!').tap { |post| attach_photo(post, photo_filenames[3]) }
+user5.posts.create!(content: 'First post!').tap { |post| attach_photo(post, photo_filenames[4]) }
+user6.posts.create!(content: 'First post!').tap { |post| attach_photo(post, photo_filenames[5]) }
+
+#Uncomment when continuing with cloudinary
+# photo_ids = [
+#   'pexels-cottonbro-4781422_xrfwon',
+#   'pexels-shkrabaanthony-4612246_ytbxrc',
+#   'pexels-ron-lach-8922193_tnvov0',
+#   'pexels-cottonbro-6530950_udruta',
+#   'pexels-cottonbro-6530941_njnqhc',
+#   'pexels-cottonbro-6530928_dxgs9h',
+#   'pexels-cottonbro-5530167_nv3rg6',
+#   'pexels-cottonbro-5530172_tzvinm',
+#   'pexels-cottonbro-5097359_rlll7t',
+#   'pexels-cottonbro-4781456_gdvxrc'
+# ]
+
+#Uncomment when continuing with cloudinary
+# def attach_photo(post, photo_id)
+#   photo_url = "https://www.pexels.com/photo/#{photo_id}"
+#   photo_file = URI.open(photo_url)
+#   post.photo.attach(io: photo_file, filename: "#{photo_id}.jpg")
+# end
+
+
+#Uncomment when continuing with cloudinary
+# user1.posts.create!(content: 'First post!', photo: )
+# user2.posts.create!(content: 'First post!', photo: )
+# user3.posts.create!(content: 'First post!', photo: )
+# user4.posts.create!(content: 'First post!', photo: )
+# user5.posts.create!(content: 'First post!', photo: )
+# user6.posts.create!(content: 'First post!', photo: )
