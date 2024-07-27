6913f4e4e0d0ee592ad05941c7f8dd0c85a15672
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-27 11:49:42 +0100

    deleted mgrations, dropped like table, re-added like table, re-did the relationships, added foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_27_101509) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_104550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_101509) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "matches", force: :cascade do |t|
@@ -93,6 +97,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_101509) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "matches", "users"
   add_foreign_key "matches", "users", column: "matched_user_id"
   add_foreign_key "posts", "users"
