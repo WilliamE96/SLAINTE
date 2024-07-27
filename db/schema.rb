50d0f5075d6a293e95b1e0a80f2aabbba1f212b5
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-27 11:16:38 +0100

    added likes table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_194750) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_101509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_194750) do
     t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "matches", force: :cascade do |t|
