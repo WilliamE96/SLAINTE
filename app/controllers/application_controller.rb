152faad7f76e63349a11426617591076d96a0994
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-30 19:32:58 +0100

    got rid of pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit::Authorization
+  # include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
