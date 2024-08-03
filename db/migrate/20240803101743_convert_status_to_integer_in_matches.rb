a479d100e07dec5430ad9dee413eee9de2ca5fc7
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-08-03 11:43:43 +0100

    fix made

--- db/migrate/20240803101743_convert_status_to_integer_in_matches.rb
+++
@@ -1,9 +0,0 @@
-class ConvertStatusToIntegerInMatches < ActiveRecord::Migration[7.1]
-  def up
-    change_column :matches, :status, :integer, using: 'status::integer'
-  end
-
-  def down
-    change_column :matches, :status, :string
-  end
-end
