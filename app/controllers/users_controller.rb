022432fac709f13b399d57c32cf5aaeb9902077f
Sabatino Catucci <sabatinocatucci@hotmail.com>
2024-08-08 19:52:04 +0100

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  has_many :matches
-  has_many :matched_users, through: :matches, source: :matched_user
 
   def show
     @posts = @user.posts.order(created_at: :desc)
